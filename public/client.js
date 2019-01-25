b1b868e7248272613e29d3dbfa8cd26e24aaeadd
Blair LaCroix <code@r0uge.org>
2019-01-25 04:17:00 -0800

    Add some chunk catching and playing

--- public/client.js
+++ public/client.js
@@ -1,8 +1,35 @@
 var socket = io('/play');
 
+var createBuffersource = (ctx, buffer) => {
+    let source = ctx.createBufferSource();
+        source.connect(ctx.destination);
+        source.buffer = buffer;
+        let duration = buffer.duration
+        source.start(0);
+        return duration
+};
+
 var main = () => {
-    
-}
+    var audioContext = new AudioContext();
+    var chunks = [];
+    var started = false;
+
+    var playNewChunk = () => {
+        let dur = createBuffersource(audioContext, chunks.shift());
+        setInterval(playNewChunk, dur);
+    }
+
+    socket.on('chunk', (data) => {
+        let decoded = audioContext.decodeAudioData(data, (buf) => {
+            chunks.push(buf);
+        });
+        if(data.length >= 60 && !started) {
+            let dur = createBuffersource(audioContext, chunks.shift());
+            setInterval(playNewChunk, dur);
+            started = true;
+        };
+    });
+};
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.addEventListener("click", main);

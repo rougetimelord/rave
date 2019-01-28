82ee2ff6e979338525cc0810fcbb48de145f4c14
Blair LaCroix <code@r0uge.org>
2019-01-28 15:46:44 -0800

    Update frontend

--- public/client.js
+++ public/client.js
@@ -1,33 +1,42 @@
 var socket = io('/play');
 
-var createBuffersource = (ctx, buffer) => {
-    let source = ctx.createBufferSource();
-        source.connect(ctx.destination);
-        source.buffer = buffer;
-        let duration = buffer.duration
-        source.start(0);
-        return duration
-};
 
 var main = () => {
-    var audioContext = new AudioContext();
-    var chunks = [];
-    var started = false;
-
-    var playNewChunk = () => {
-        let dur = createBuffersource(audioContext, chunks.shift());
-        setInterval(playNewChunk, dur);
-    }
+    document.body.removeEventListener("click", main);
+    var ctx = new AudioContext();
+    var chunks = []
+    var nextPlay = 0;
+    var currentPlay = 0;
 
     socket.on('chunk', (data) => {
-        let decoded = audioContext.decodeAudioData(data, (buf) => {
-            chunks.push(buf);
-        });
-        if(data.length >= 60 && !started) {
-            let dur = createBuffersource(audioContext, chunks.shift());
-            setInterval(playNewChunk, dur);
-            started = true;
-        };
+        console.log('got chunk');
+
+        let channel0 = new Float32Array(data.left);
+        let channel1 = new Float32Array(data.right);
+
+        let buffer = ctx.createBuffer(2, channel0.length, 44000)
+
+        let a = buffer.getChannelData(0);
+        let b = buffer.getChannelData(1);
+        for(let i = 0; i < channel0.length; i++){
+            a[i] = channel0[i];
+            b[i] = channel1[i];
+        }
+
+        chunks.push(buffer);
+
+        if (chunks. length >= 25) {
+            let bufferSource = ctx.createBufferSource()
+
+            bufferSource.buffer = chunks.shift();
+
+            bufferSource.connect(ctx.destination);
+
+            bufferSource.start(ctx.currentTime + nextPlay);
+
+            currentPlay = ctx.currentTime;
+            nextPlay = buffer.duration;
+        }
     });
 };
 
